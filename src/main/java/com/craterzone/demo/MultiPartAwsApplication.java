c9c49c8bdd63e9cfd4f79f90f86a1c22a93c6c91
niks1610 <nikhilkalani486@gmail.com>
2021-02-22 10:58:53 +0530

    Upload Multipart Using S3

---
+++ src/main/java/com/craterzone/demo/MultiPartAwsApplication.java
@@ -0,0 +1,13 @@
+package com.craterzone.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiPartAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiPartAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/craterzone/demo/MultiPartAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.craterzone.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiPartAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
